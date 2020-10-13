e4b769ae6e00e0a95154f957d0a523c9d70f7760
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-13 14:36:42 -0400

    feat: implement morse decoder package

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let lex
+    let result = ''
+    let phrase = expr.split('**********')
+    for (let i = 0; i < phrase.length; i++) {
+        let word = []
+        for (let j = 0, m = 0; j < phrase[i].length; j += 10, m++) {
+            word.push(phrase[i].slice(j, j + 10))
+            str1 = word[m].replace(/[^1]+/, '').replace(/11/g, '-').replace(/10/g, '.')
+            str2 = word[m].replace(/11/g, '-').replace(/10/g, '.')
+            MORSE_TABLE.hasOwnProperty(str1) ? word[m] = MORSE_TABLE[str1] : word[m] = MORSE_TABLE[str2]            
+        }
+        result += ' ' + word.join('')
+    }
+    return result.trim()
 }
 
 module.exports = {
